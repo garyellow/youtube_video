abf2c3094e6733b2618a421a1d5614b5d2cc85d4
黃冠穎 <gary20011110@gmail.com>
2021-10-03 12:56:21 +0800

    upgrade pytube and change the language

--- yt_downloader.py
+++ yt_downloader.py
@@ -6,80 +6,80 @@ from pytube import exceptions
 
 download_dir = 'download'
 
-print('歡迎使用YouTube影片下載器～～\n')
-print('檔案的預設儲存位置為與此程式同資料夾的「download」資料夾')
-print('按enter鍵繼續，其他鍵變更儲存位置......')
+print('This is YouTube video downloader~~\n')
+print('Default storage location for files is "download" folder')
+print('Press enter key to continue, other keys to change the storage location...')
 
 if ord(msvcrt.getch()) != 13:
     while True:
-        download_dir = input('\n請輸入儲存位置\n>> ')
+        download_dir = input('\nEnter the storage location you want\n>> ')
         if len(download_dir) != 0:
             break
 
 while True:
-    URL = input('\n請輸入影片網址\n>> ')
+    URL = input('\nEnter the URL of the video\n>> ')
     if len(URL) == 0:
-        print('此項必填')
+        print('This is required!')
         continue
 
     try:
         yt: YouTube = YouTube(URL)
     except exceptions.RegexMatchError:
-        print('網址錯誤')
+        print('URL is fault, please check it')
         continue
 
     while True:
-        download_kind = input('\n是否僅需下載音檔(y/n)\n>> ')
-        print('\n開始下載～～')
+        download_kind = input('\nOnly audio? (y/n)\n>> ')
+        print('\nStart download, please wait for a short time~~')
 
         if download_kind == 'y' or download_kind == 'Y':
             try:
                 yt.streams.get_audio_only().download(download_dir)
             except error.URLError:
-                print('\n無法連線至網際網路\n')
+                print('\nUnable to connect to the internet\n')
                 break
             except exceptions.AgeRestrictedError:
-                print('\n無法下載設有年齡限制的影片\n')
+                print('\nUnable to download age-restricted videos\n')
                 break
             except exceptions.LiveStreamError:
-                print('\n無法下載直播中的影片\n')
+                print('\nUnable to download the video in the live\n')
                 break
             except exceptions.MembersOnly:
-                print('\n無法下載' + yt.author + '的會員專屬影片\n')
+                print('\nUnable to download ' + yt.author + '\'s member-only videos\n')
                 break
             except exceptions:
-                print('\n未知異常\n')
+                print('\nUnknown exception\n')
                 break
             else:
-                print('\n\n影片名稱：' + yt.title)
-                print('上傳者：' + yt.author)
-                print('\n音檔下載成功～\n\n')
+                print('\n\nVideo name：' + yt.title)
+                print('Uploader：' + yt.author)
+                print('\nAudio file downloaded successfully~\n\n')
                 break
 
         if download_kind == 'n' or download_kind == 'N':
             try:
                 yt.streams.get_highest_resolution().download(download_dir)
             except error.URLError:
-                print('\n無法連線至網際網路\n')
+                print('\nUnable to connect to the internet\n')
                 break
             except exceptions.AgeRestrictedError:
-                print('\n無法下載設有年齡限制的影片\n')
+                print('\nUnable to download age-restricted videos\n')
                 break
             except exceptions.LiveStreamError:
-                print('\n無法下載直播中的影片\n')
+                print('\nUnable to download the video in the live\n')
                 break
             except exceptions.MembersOnly:
-                print('\n無法下載' + yt.author + '的會員專屬影片\n')
+                print('\nUnable to download ' + yt.author + '\'s member-only videos\n')
                 break
             except exceptions:
-                print('\n未知異常\n')
+                print('\nUnknown exception\n')
                 break
             else:
-                print('\n\n影片名稱：' + yt.title)
-                print('上傳者：' + yt.author)
-                print('\n影片下載成功～\n\n')
+                print('\n\nVideo name：' + yt.title)
+                print('Uploader：' + yt.author)
+                print('\nVideo download successfully~\n')
                 break
 
-    print('按Enter鍵下載其他影片，其他鍵關閉......')
+    print('Press Enter to download other videos, other keys to close...')
     if ord(msvcrt.getch()) != 13:
         break
